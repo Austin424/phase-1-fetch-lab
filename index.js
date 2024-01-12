86d6d47bc494e9611446972249d958fc024f9b65
Austin Rivera <austin.rivera.24@aol.com>
2024-01-12 08:46:59 -0500

    fetch lab

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
-
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -10,6 +5,17 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+}
+
+function fetchBooks() {
+  // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(renderBooks)
+    .catch(error => {
+      console.error('Error fetching books:', error);
+      // Handle the error appropriately (throw, log, etc.)
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
